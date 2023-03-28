92d26b07971349326bb05039ff0dffe64af46953
humanbeing <egeatmaca98@gmail.com>
2023-03-28 21:48:17 +0200

    get_prices implemented

--- PriceScraper.py
+++ PriceScraper.py
@@ -6,13 +6,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 # from webdriver_manager.chrome import ChromeDriverManager
+from typing import Generator
+from enum import Enum
 
+class MarketPlace(Enum):
+    AMAZON = "https://www.amazon.com/"
+    EBAY = "https://www.ebay.com/"
+    BESTBUY = "https://www.bestbuy.com/"
 
 class PriceScraper:
-    def __init__(self, search_query):
-        self.url = "https://www.amazon.com/"
-        self.search_query = search_query
-        self.driver = self.get_driver()
+    def __init__(self, marketplace):
+        self.marketplace = marketplace
+        self.url = marketplace.value
 
     def get_options(self):
         chrome_options = Options()
@@ -26,43 +31,54 @@ class PriceScraper:
 
         return chrome_options
 
-    def get_driver(self):
-        driver = webdriver.Chrome(options=self.get_options())
+    def create_driver(self):
+        service = ChromeService("chromedriver/chromedriver")
+        # driver = webdriver.Chrome(service=service) # For local
+        driver = webdriver.Chrome(options=self.get_options()) # For docker
 
         return driver
     
-    def get_search_links(self):
-        self.driver.get(self.url)
-        search_bar = self.driver.find_element(By.ID, "twotabsearchtextbox")
-        search_bar.send_keys(self.search_query)
+    def get_search_links(self, search_query:str) -> Generator:
+        driver = self.create_driver()
+
+        driver.get(self.url)
+
+        search_bar = driver.find_element(By.ID, "twotabsearchtextbox")
+        search_bar.send_keys(search_query)
         search_bar.send_keys(Keys.RETURN)
 
         try:
-            search_results = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located(By.CSS_SELECTOR, "div.s-result-item")
-            )
-            search_links = [result.find_element(By.CSS_SELECTOR, "a.a-link-normal").get_attribute("href") for result in search_results]
-            return search_links
-        except:
-            self.driver.quit()
+            search_results = driver.find_elements(
+                By.CSS_SELECTOR, "div.s-result-item a.a-link-normal")
+            for result in search_results:
+                yield result.get_attribute("href")
+        except Exception as e:
+            print(e)
+            
+        driver.quit()
 
-    def get_prices(self, links):
-        for link in links:
-            self.driver.get(link)
+    def get_prices(self, search_query: str) -> Generator:
+        driver = self.create_driver()
+
+        search_link_generator = self.get_search_links(search_query)
+        for link in search_link_generator:
+            print('\nlink: ', link)
+            driver.get(link)
             try:
-                price = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located(By.ID, "priceblock_ourprice")
-                )
-                yield price.text
+                price_whole = driver.find_element(By.CLASS_NAME, "a-price-whole")
+                price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+                price_symbol = driver.find_element(By.CLASS_NAME, "a-price-symbol")
+                value = float(price_whole.text + "." + price_fraction.text)
+                currency = price_symbol.text
+                price = {"value": value, "currency": currency}
+                print('price: ', price)
+                yield price
             except Exception as e:
                 print(e)
 
-    def quit(self):
-        self.driver.quit()
+        driver.quit()
 
 if __name__ == "__main__":
-    scraper = PriceScraper("laptop")
-    links = scraper.get_search_links()
-    print(links)
-    print(scraper.get_prices(links))
-    scraper.quit()
+    scraper = PriceScraper(MarketPlace.AMAZON)
+    search_query = "laptop"
+    print([price for price in scraper.get_prices(search_query)])
