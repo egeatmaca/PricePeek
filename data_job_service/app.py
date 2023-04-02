9cc74a64c31afdb11cab71cefb01211ad3bca61d
humanbeing <egeatmaca98@gmail.com>
2023-04-02 20:07:41 +0200

    minor changes

--- data_job_service/app.py
+++ data_job_service/app.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 import uvicorn
-import logging
+from jobs import scrape_and_analyze
+from scrape.ScraperFactory import Marketplace
 from logs.log_config import config_logs
-from scripts import scrape_and_analyze
 
 app = FastAPI()
 
 @app.get("/{marketplace}/{search_query}")
 def index(marketplace, search_query):
-    logging.info(f"Started scraping and analyzing {search_query} on {marketplace}...")
-    scrape_and_analyze(marketplace, search_query)
-    logging.info(f"Finished scraping and analyzing {search_query} on {marketplace}")
+    marketplace_enum = None
+    if marketplace == "amazon":
+        marketplace_enum = Marketplace.AMAZON
+    elif marketplace == "ebay":
+        marketplace_enum = Marketplace.EBAY
+    elif marketplace == "etsy":
+        marketplace_enum = Marketplace.ETSY
+    else:
+        return {"message": "Marketplace not found"}
+
+    scrape_and_analyze(marketplace_enum, search_query)
+
     return {"message": "success"}
 
 if __name__ == "__main__":
